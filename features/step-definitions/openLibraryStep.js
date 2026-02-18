9b2b509bae4f766e0e2e4eea16e076051ba1d444
minalpatelsyd <minalpatelsyd@gmail.com>
2026-02-18 21:38:16 +1100

    updated

--- features/step-definitions/openLibraryStep.js
+++ features/step-definitions/openLibraryStep.js
@@ -1,24 +1,20 @@
 const { Given, Then } = require('@cucumber/cucumber');
 const { request, expect } = require('@playwright/test');
 
+let response;
 let apiContext;
-let responseBody;
 
-// Step definitions for OpenLibrary API tests
-//to run the tests, use the command: npm test
-
-Given('I will send a GET request to the OpenLibrary author endpoint', async function () {
-    apiContext = await request.newContext();
-    const response = await apiContext.get('https://openlibrary.org/authors/OL1A.json');
-    
-    expect(response.status()).toBe(200);
-    responseBody = await response.json();
+Given('I send a GET request to the OpenLibrary author endpoint', async function () {
+  apiContext = await request.newContext();
+  response = await apiContext.get('https://openlibrary.org/authors/OL1A.json');
 });
 
 Then('the personal_name should be {string}', async function (expectedName) {
-    expect(responseBody.personal_name).toBe(expectedName);
+  const body = await response.json();
+  expect(body.personal_name).toBe(expectedName);
 });
 
 Then('the alternate_names should contain {string}', async function (expectedAltName) {
-    expect(responseBody.alternate_names).toContain(expectedAltName);
+  const body = await response.json();
+  expect(body.alternate_names).toContain(expectedAltName);
 });
